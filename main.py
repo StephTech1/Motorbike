7382b5caa368db0eac3a3636c45495dedfac4cda
Steph St <steph.streete@outlook.com>
2020-09-29 08:19:39 +0000

    added code

---
+++ main.py
@@ -0,0 +1,17 @@
+product_name = "Motorbike"
+product_value = 2000
+print(product_name,product_value)
+
+deduction = product_value / 100 * 10
+
+year1=(product_value-deduction)
+
+print(year1)
+year2= year1 - (year1 / 100 * 10)
+print(year2)
+year3= year2 - (year2 / 100 *10)
+print(year3)
+year4= year3 - (year3 / 100 *10)
+print(year4)
+year5= year4 - (year4 / 100 *10)
+print(year5)
