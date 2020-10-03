8af49f91c8f31c666f959a3c42e12cb0a23fdc55
Steph St <steph.streete@outlook.com>
2020-10-03 11:49:24 +0000

    Added colons and £ signs

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 print ("Motorbike Value")
+print ("Each month value decreases by 10%")
 motorbike = 2000
 print("£",motorbike)
 
 while motorbike > 1000:
   motorbike -= motorbike * 0.1
-  print(motorbike)
+  print("£",motorbike)
 
 else:
     print("Less than £1000")
